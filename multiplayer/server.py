e70781333101136415b896e775667717bb4f4561
kevrap <180104258+kevrap@users.noreply.github.com>
2025-03-19 02:27:59 -0400

    Add files via upload

---
+++ multiplayer/server.py
@@ -0,0 +1,124 @@
+#
+# TIC TAC TOE server.py
+# CNT4007 Project by Fareed Fareed-Uddin, Khizer Butt, Kevin Rapkin, Pedro Mantese, Juan Martinez
+# version 1 on 3/18/2025 - Kevin Rapkin
+#
+#
+#
+#
+
+import pygame
+import socket
+import threading
+from board import Board
+
+# set window parameters
+surface = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('TIC TAC TOE')
+
+# create a thread
+def create_thread(target):
+    thread = threading.Thread(target = target)
+    # set it as daemon thread so it's killed automatically when game is closed
+    thread.daemon = True
+    thread.start()
+
+
+
+# host is set to local network by default, and port is chosen to be 12345
+HOST = '127.0.0.1'
+PORT = 12345
+connected = False
+connecton, address = None, None
+
+# create socket, default IPV4 = AF_INET and TCP + SOCK_STREAM
+sock = socket.socket()
+# bind socket to host and port
+sock.bind((HOST, PORT))
+# listening to 1 connection, because two players connected through one connection
+sock.listen(1)
+
+# receive data from client
+def receive_data():
+    global turn
+    while True:
+        # receive byte of data and decode it
+        data = connection.recv(1024).decode()
+        # splits data after every -
+        data = data.split('-')
+        x = int(data[0])
+        y = int(data[1])
+        # check if data received indicates server's turn
+        if data[2] == 'yourturn':
+            turn = True
+        # check if game is over
+        if data[3] == 'False':
+            board.gameover = True
+        if board.get_cell_val(x, y) == 0:
+            board.set_cell_val(x, y, 'O')
+        # show data receieved in console
+        print(data)
+
+# waits for connection to client
+def waiting_to_connect():
+    global connected, connection, address
+    # this is where it accepts a connection, otherwise the program waits
+    connection, address = sock.accept()
+    print('Connection Active')
+    connected = True
+    receive_data()
+
+# runs waiting to connect function in separate thread to prevent game from becoming unresponsive
+create_thread(waiting_to_connect)
+
+
+running = True
+
+board = Board()
+
+# initialize user as X going first
+user = "X"
+
+# turn starts on server side
+turn = True
+playing = 'True'
+
+# main loop
+while running:
+
+    for event in pygame.event.get():
+        # press X to close game
+        if event.type == pygame.QUIT:
+            running = False
+
+        # left click event
+        if event.type == pygame.MOUSEBUTTONDOWN and connected:
+            if pygame.mouse.get_pressed()[0]:
+                # check if turn is active and game isn't over
+                if turn and not board.gameover:
+                    # mouse click position on board
+                    position = pygame.mouse.get_pos()
+                    cell_X, cell_Y = position[0] // 200, position[1] // 200
+                    # divides position by 200 to separate into 3 x 3 grid matrix on 600 x 600 pixel board with 2 pixel width gridlines
+                    board.get_mouse_input(cell_X, cell_Y, user)
+                    # check if game is over before sending more data
+                    if board.gameover:
+                        playing = 'False'
+                    # prepare cell_X and cell_Y data and encode into byte string to send through TCP connection
+                    send_data = '{}-{}-{}-{}'.format(cell_X, cell_Y, 'yourturn', playing).encode()
+                    # send data to client
+                    connection.send(send_data)
+                    turn = False
+               
+
+        # space bar to clear board at gameover
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and board.gameover:
+                board.clear_board()
+                board.gameover = False
+                playing = 'True'
+
+    # board background color = cream(255,253,208)
+    surface.fill((255, 253, 208))
+    board.draw(surface)
+    pygame.display.flip()
